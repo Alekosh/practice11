d748d68f2465314cee516557c227ca07ba29de5a
Alekosh <aakhosh00@gmail.com>
2023-09-09 01:21:52 +0500

    added more section

--- js/main.js
+++ js/main.js
@@ -82,3 +82,80 @@ if (iconMenu) {
 // 		}
 // 	}
 // }
+
+// ==============================================================
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  //loop: true, // true - круговой слайдер, false - слайдер с конечными положениями
+  // speed: 500,
+  // effect: 'slider', // cards, coverflow, flip, fade, cube
+  // initialSlide: 1, // Начинаем со 2 слайдера
+  // freeMode: true, // Можно перетаскивать как ленту
+  slidesPerView: 3, // кол-во активных слайдов 1 || 2 || 3 || 1.5 || "auto"
+  centeredSlider: true, // центрирование слайдер
+  spaceBetween: 24, // расстояние между слайдами
+  // slidesPerGroup: 3, // кол-во пролистываемых слайдов
+
+  // пагинация в виде точек
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true, // true - пагинация становится кликабельной
+  // },
+
+  // кнопки вперед и назад
+  navigation: {
+    prevEl: '.swipper__prev',
+    nextEl: '.swipper__next',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+
+  // Автовоспроизведение
+  // autoplay: {
+  //   delay: 1000,
+  // },
+
+  // адаптив
+  breakpoints: {
+    1251: { //проблемы с кнопкой next
+      spaceBetween: 20,
+      slidesPerView: 3,
+    },
+
+    951: {
+      spaceBetween: 20,
+      slidesPerView: 2,
+    },
+
+    0: {
+      spaceBetween: 0,
+      slidesPerView: 1,
+    },
+    //============ нужно прописывать в медиа в css //============//
+    // @media (max-width: 1250px) {
+    //   .swiper, swiper-wrapper {
+    //     max-width: 780px;
+    //   }
+    // }
+
+    // @media (max-width: 950px) {
+    //   .swiper, swiper-wrapper {
+    //     max-width: 320px;
+    //   }
+    // }
+  },
+});
+
+const swiperPrev = document.querySelector('.swiperPrev');
+const swiperNext = document.querySelector('.swiperNext');
+
+swiperPrev.addEventListener('click', () => {
+  swiper.slidePrev();
+})
+swiperNext.addEventListener('click', () => {
+  swiper.slideNext();
+})
